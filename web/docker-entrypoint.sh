fb74a7e18449413b9ec7c88e24ee87c234a51397
billchenchina <billchenchina2001@gmail.com>
2022-11-06 00:51:05 +0800

    Add /var/uoj/config.php

--- web/docker-entrypoint.sh
+++ web/docker-entrypoint.sh
@@ -2,13 +2,18 @@
 
 set -Eeuo pipefail
 
-if [ -d /var/www/uoj ]; then
+if [ -f /var/www/uoj/index.php ]; then
     # Develop environment
     find /etc/apache2/ -type f -name "*.conf" -exec sed -i "s|/usr/src|/var/www|g" {} +
 fi
 
 mkdir -p /var/uoj/data/upload/
 mkdir -p /var/uoj/storage/{submission,tmp}/
+
+if [ ! -f /usr/src/uoj/app/.config.php ]; then
+    cp /usr/src/uoj/app/.default-config.php /var/uoj/config.php
+    ln -s /var/uoj/config.php /usr/src/uoj/app/.config.php
+fi
 
 chown -R www-data:www-data /var/uoj
 
